77b8313c120e9306d4d84da73f1d8c9815ae5d12
piandv <piandv@piandv-HP-1000-Notebook-PC>
2015-05-15 14:54:54 +0700

    comit15/5pm

--- maven-demo/src/main/java/demo/controller/DemoController.java
+++ maven-demo/src/main/java/demo/controller/DemoController.java
@@ -1,30 +1,18 @@
 package demo.controller;
-
-
-
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
+@RequestMapping("/")
+public class DemoController {
 
-
-
-@RequestMapping({"/model"})
-public class ModelController {
-	
-	
-	
 	@RequestMapping("")
-	public String getPageModel(){
-		
-		return "model/index.isp";
-	}
-	
-	@RequestMapping("home")
 	public String getPageHome(){
+		
 		return "home.xhtml";
 	}
-	
+	@RequestMapping("idx")
+	public String getPageIndex(){
+		return "index.jsp";
+	}
 }
